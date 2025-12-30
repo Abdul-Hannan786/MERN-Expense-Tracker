29679858d8089544eccdc8256b0f0977d38621b6
Abdul Hannan <abdulhannanhere@gmail.com>
2025-12-31 01:15:23 +0500

    update in Login Page

---
+++ Frontend/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+        {children}
+      </div>
+
+      <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+        <div className="w-48 h-48 rounded-[40px] bg-purple-600"></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
