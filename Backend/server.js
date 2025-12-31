9c71d30e56e15c9f86629d255952e38bd6b72e1b
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-01 00:43:30 +0500

    Backend started

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
