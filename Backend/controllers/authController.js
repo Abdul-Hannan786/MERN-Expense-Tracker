40645ac5c23a2e4f1d7ba4273540ebd70c04b0ff
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-02 02:30:24 +0500

    Api Endpoint created

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -76,7 +76,7 @@ export const getUserInfo = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(401).json({ message: "User not found" });
     }
 
     res.status(200).json(user);
