21af1bc7a36076a5864274e825d4b11b996d3f9a
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-01 19:45:21 +0500

    Add Income API created

--- Backend/controllers/incomeController.js
+++ Backend/controllers/incomeController.js
@@ -1,5 +1,30 @@
+import Income from "../models/Income.js"
+
 // Add Income Source
-export const addIncome = async (req, res) => {};
+export const addIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { icon, source, amount, date } = req.body;
+
+    // Validation: Check for missing fields
+    if (!source || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // Get All Income Source
 export const getAllIncome = async (req, res) => {};
--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -10,6 +10,7 @@ export const protect = async (req, res, next) => {
   }
 
   const token = req.headers.authorization.split(" ")[1];
+  // console.log(token)
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
