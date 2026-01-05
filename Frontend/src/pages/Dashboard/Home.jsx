3c89dce00bee926c235c8badbe1cef256f18922c
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-06 03:22:40 +0500

    Sidemenu completed

--- Frontend/src/pages/Dashboard/Home.jsx
+++ Frontend/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Dashboardlayout from "../../components/layouts/Dashboardlayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
 
 const Home = () => {
+  useUserAuth()
   return (
     <Dashboardlayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">Home</div>
