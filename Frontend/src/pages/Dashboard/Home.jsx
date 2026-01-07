98b428d5b8977a417c5e7ea1a47c11fb16234a92
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-07 07:30:08 +0500

    Dashboard Data fetched

--- Frontend/src/pages/Dashboard/Home.jsx
+++ Frontend/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dashboardlayout from "../../components/layouts/Dashboardlayout";
 import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Home = () => {
-  useUserAuth()
+  useUserAuth();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.GET_DATA);
+      if (response.data) {
+        setDashboardData(response.data);
+        console.log(response.data)
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData()
+    return () => {};
+  }, []);
+
   return (
     <Dashboardlayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">Home</div>
