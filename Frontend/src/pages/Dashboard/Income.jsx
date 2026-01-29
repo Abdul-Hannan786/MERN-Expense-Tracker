ec87af09564f9026595c6efd696dee89f30c52df
Abdul Hannan <abdulhannanhere@gmail.com>
2026-01-30 04:15:12 +0500

    Started working on income page

--- Frontend/src/pages/Dashboard/Income.jsx
+++ Frontend/src/pages/Dashboard/Income.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import Dashboardlayout from "../../components/layouts/Dashboardlayout";
+import IncomeOverview from "../../components/Income/IncomeOverview";
 
 const Income = () => {
-  return (
-    <div>Income</div>
-  )
-}
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    date: null,
+  });
 
-export default Income
+  // Get All Income Details
+  const fetchIncomeDetails = async () => {};
+
+  // Handle Add Income
+  const handleAddIncome = async (income) => {};
+
+  // Delete Income
+  const deleteIncome = async (id) => {};
+
+  // handle Download income details
+  const handleDownloadIncomeDetails = async () => {}
+
+  return (
+    <Dashboardlayout activeMenu="Income">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <IncomeOverview
+              transactions={incomeData}
+              onAddIncome={() => setOpenAddIncomeModal(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </Dashboardlayout>
+  );
+};
+
+export default Income;
